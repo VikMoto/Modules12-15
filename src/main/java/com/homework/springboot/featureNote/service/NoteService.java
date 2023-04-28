07f6bd76674e4fe38d617ab94eafd94a80a40ca2
ig.basalt@gmail.com <ig.basalt@gmail.com>
2023-04-28 22:34:06 +0300

    module 12

---
+++ src/main/java/com/homework/springboot/featureNote/service/NoteService.java
@@ -0,0 +1,45 @@
+package com.homework.springboot.featureNote.service;
+
+import com.homework.springboot.featureNote.entity.Note;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+public class NoteService {
+    private static Map<Long, Note> notes = new HashMap<>();
+    public Note add(Note note) {
+        Long id;
+//        do {
+          id = (long) (Math.random()/Math.nextDown(1.0) * 3000400002304L);
+//        }while (notes.get(id) == null);
+
+        note.setId(id);
+        notes.put(id, note);
+        return notes.get(id);
+    }
+
+    public Collection<Note> listAll() {
+        Collection<Note> values = notes.values();
+        return  values;
+    }
+
+    public Note deleteById(Long id) {
+        return notes.remove(id);
+    }
+
+
+    public Note getById(Long id) {
+        return notes.get(id);
+    }
+
+    public void update(Note note) {
+        Optional<Note> note1 = Optional.ofNullable(notes.get(note.getId()));
+        if (note1.isPresent()) {
+            note1.get().setTitle(note.getTitle());
+            note1.get().setContent(note.getContent());
+        } else {
+            throw new RuntimeException();
+        }
+    }
+}
---
+++ src/test/java/com/homework/springboot/featureNote/service/NoteServiceTest.java
@@ -0,0 +1,80 @@
+package com.homework.springboot.featureNote.service;
+
+import com.homework.springboot.featureNote.entity.Note;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.*;
+
+import java.util.Collection;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class NoteServiceTest {
+
+    private static NoteService noteService;
+    private static Collection<Note> notes;
+
+
+    @BeforeAll
+    public static void init(){
+        noteService = new NoteService();
+    }
+
+    @Test
+    @Order(1)
+    void add() {
+        Note note = new Note().builder().title("Todo").content("Buy milk").build();
+        Note addedNote = noteService.add(note);
+        System.out.println("addedNote.getId() = " + addedNote.getId());
+        assertEquals(note.getTitle(), addedNote.getTitle());
+    }
+
+    @Test
+    @Order(2)
+    void listAll() {
+        Note note = new Note().builder().title("Todo2").content("Buy sugar").build();
+        Note addedNote = noteService.add(note);
+        notes = noteService.listAll();
+        assertEquals(2, notes.size());
+    }
+
+    @Test
+    @Order(3)
+    void deleteById() {
+        Note note = new Note().builder().title("Todo3").content("Buy mango").build();
+        Note addedNote = noteService.add(note);
+
+        Long id = addedNote.getId();
+        Note deleteById = noteService.deleteById(id);
+        assertEquals(addedNote, deleteById);
+    }
+
+    @Test
+    @Order(4)
+    void getById() {
+        Note note = new Note().builder().title("Todo3").content("Buy mango").build();
+        Note addedNote = noteService.add(note);
+        Long id = addedNote.getId();
+
+        Note byId = noteService.getById(id);
+        System.out.println("notes = " + notes);
+        assertEquals(addedNote, byId);
+
+    }
+
+    @Test
+    @Order(5)
+    void update() {
+        Note note = notes.stream().findFirst().get();
+        Long id = note.getId();
+        String newContent = "Sell house";
+
+        note.setContent(newContent);
+        noteService.update(note);
+        Note noteAfterUpdate = noteService.getById(id);
+
+        assertEquals(newContent,noteAfterUpdate.getContent());
+    }
+}
